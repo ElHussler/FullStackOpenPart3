ab78e18ab8ac22781cced44bd00d70973aaa6c1f
elhussler <animanic@hotmail.co.uk>
2024-06-05 15:04:04 +0100

    exercise 3.19

--- index.js
+++ index.js
@@ -72,14 +72,13 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 //USES DATABASE
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(
+    request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
       if (updatedPerson)
         response.json(updatedPerson)
@@ -92,13 +91,7 @@ app.put('/api/persons/:id', (request, response, next) => {
 //USES DATABASE
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number is missing'
-    })
-  }
-
+  
   // if (persons.find(p => p.name === body.name)) {
   //   return response.status(400).json({
   //     error: 'name must be unique'
@@ -140,6 +133,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
